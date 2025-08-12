08bf17230d85a8b19a48d7e7eab9eb6f780ef171
SheetalShah12 <shahsheetal160@gmail.com>
2025-08-12 22:15:12 +0530

    server changes

--- backend/server.js
+++ backend/server.js
@@ -37,5 +37,5 @@ app.post("/submit", (req, res) => {
 app.use("/api", verificationRoutes);
 app.use("/api", panRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
